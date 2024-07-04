a7b21e63b17a3e7123594405ea6d67dcf635308a
Riki225 <79812982+Riki225@users.noreply.github.com>
2024-07-05 02:27:48 +1000

    Added Reusable Modal - passing data using props

---
+++ src/components/Modal.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Modal({ show, onClose, children }) {
+  return (
+    <div>
+      {show && (
+        <div className="absolute top-0 left-0 h-full w-full z-10">
+          <div className="container bg-slate-300 border-2 border-black mx-auto max-w-2xl h-[80vh] rounded-3xl mt-12 ">
+            <div className="w-full flex flex-row justify-end p-4">
+              <button
+                onClick={() => onClose(!show)}
+                className="w-10 h-10 rounded-full bg-red-500 border-gray-600 border-2 text-white font-bold"
+              >
+                X
+              </button>
+            </div>
+            {children}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
