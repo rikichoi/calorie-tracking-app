3fcd1c43a5a62b2a0c40b00b48aa53a08fa8d5c6
Riki225 <79812982+Riki225@users.noreply.github.com>
2024-07-08 04:06:34 +1000

    Added title - TODO: Form validation for Add meal and Add exercise

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,9 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>HealthDiary</title>
+      </head>
       <body>
         <AuthContextProvider>
           <Navbar />
