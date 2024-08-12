9fe2e0c850f6dac7d1b5240ada4f61e5fa7124ee
Riki225 <79812982+Riki225@users.noreply.github.com>
2024-08-12 21:22:25 +1000

    Added monitoring

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,8 +5,8 @@ import Navbar from "@/components/Navbar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ViewContextProvider from "@/lib/store/view-context";
-
 import AuthContextProvider from "@/lib/store/auth-context";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout({ children }) {
   return (
@@ -23,6 +23,7 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"></link>
       </head>
       <body className="bg-gray-50">
+        <Analytics/>
         <AuthContextProvider>
           <ViewContextProvider>
           <ToastContainer />
